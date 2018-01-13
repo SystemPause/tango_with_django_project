09172f3b7401808a5e0be1a3b560e07095cb18d5
Alessandro Speggiorin <2268690s@student.gla.ac.uk>
2018-01-13 18:27:29 +0000

    About section added to views.py and urls.py

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner")
+    return HttpResponse("Rango says hey there partner. <a href='/rango/about/'>Go to about</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Go to index</a>")
