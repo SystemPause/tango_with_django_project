5426e9eb086bc47af317c82d6648dbad24dda41a
Alessandro Speggiorin <2268690s@student.gla.ac.uk>
2018-01-17 23:45:52 +0000

    Static and media folder/files added. The about page has been implemented by creating a custom template (about.html)

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,6 @@ def index(request):
     return render(request, 'rango/index.html',context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Go to index</a>")
+    context_dict = {'boldmessage':'This tutorial has been put together by Alessandro Speggiorin.'}
+    return render(request, 'rango/about.html',context=context_dict)
+
